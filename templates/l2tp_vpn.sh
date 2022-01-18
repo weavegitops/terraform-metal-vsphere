3e5b08a8d04f5604523b94ed05b9c4bb19d407d3
Darryl Weaver <darryl@weave.works>
2022-01-18 12:14:44 +0000

    get rid of extra space

--- templates/l2tp_vpn.sh
+++ templates/l2tp_vpn.sh
@@ -414,7 +414,7 @@ if [ "$ipt_flag" = "1" ]; then
   # Uncomment if you wish to disallow traffic between VPN clients themselves
   # iptables -I FORWARD 2 -i ppp+ -o ppp+ -s "$L2TP_NET" -d "$L2TP_NET" -j DROP
   # iptables -I FORWARD 3 -s "$XAUTH_NET" -d "$XAUTH_NET" -j DROP
-  # iptables -A FORWARD -j DROP
+  #iptables -A FORWARD -j DROP
   iptables -t nat -I POSTROUTING -s "$XAUTH_NET" -o "$NET_IFACE" -m policy --dir out --pol none -j MASQUERADE
   iptables -t nat -I POSTROUTING -s "$L2TP_NET" -o "$NET_IFACE" -j MASQUERADE
   iptables -t nat -I POSTROUTING -s 172.16.0.0/24 -o "$NET_IFACE" -j MASQUERADE
